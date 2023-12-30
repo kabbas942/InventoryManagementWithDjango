92880a903f26e8765be6eda85e5d728eede58fdc
Muhammad Abbas <kabbas942@gmail.com>
2023-12-30 21:47:02 +0500

    11th Commit

--- Account/urls.py
+++ Account/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from Account import views
 from django.contrib.auth.views import LoginView
 urlpatterns = [
-    path('',LoginView.as_view(template_name='Account/login.html'),name="user-login")
+    path('',LoginView.as_view(template_name='Account/login.html'),name="user-login"),
+    path('signup',views.signup,name="signup")
 ]
--- Account/views.py
+++ Account/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .forms import userform
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout,Field,Submit
 # Create your views here.
+
+def signup(request):
+    form= userform()
+    return render(request,"Account/signup.html",{'form':form})
